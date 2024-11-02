4e4b6a787e242615571e9ff8e7db62aed64a4774
Zach Johnson <zacharyadamsjohnson@gmail.com>
2024-11-02 10:43:37 -0400

    Create initial_setup.sh

---
+++ initial_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/FalseProfit/Linux.git
+mv Linux Zach_Linux_Setup_Files
+
+
