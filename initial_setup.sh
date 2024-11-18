f66948c2b2a78412448443f76704f807a60c8e87
Zach Johnson <zacharyadamsjohnson@gmail.com>
2024-11-18 10:21:53 -0500

    Update initial_setup.sh
    Added moving configuration files into place.

--- initial_setup.sh
+++ initial_setup.sh
@@ -3,7 +3,14 @@
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+#Grab Zach's Linux configuration files
 git clone https://github.com/FalseProfit/Linux.git
+#Move config files into place
 mv Linux Zach_Linux_Setup_Files
+mv Zach_Linux_Setup_Files/.tmux.conf ~/.tmux.conf
+mv Zach_Linux_Setup_Files/.zshrc ~/.zshrc
+mv Zach_Linux_Setup_Files/zach.zsh-theme ~/.oh-my-zsh/themes/zach.zsh-theme
 
+#Apply changes to zsh
+source ~/.zshrc
 
