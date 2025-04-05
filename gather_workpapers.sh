1776d3d105963aa469c05d6392a027e9390ad059
Zach Johnson <zacharyadamsjohnson@gmail.com>
2025-04-05 16:25:39 -0400

    Update gather_workpapers.sh
    Added TODOs

--- gather_workpapers.sh
+++ gather_workpapers.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 #Author: Zach Johnson
 
+#TODO:
+#  - Add argument to take in the workpapers folder path
+#  - Add all typical automatically generated workpaper locations to be copied or moved into the user provided workpapers folder path
+
+
+
 cp -r ~/.nxc/logs $1
