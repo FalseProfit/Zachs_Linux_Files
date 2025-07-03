531502d6578746ee98085c2cf8b8fb4e726c965c
Zach <zacharyadamsjohnson@gmail.com>
2025-07-03 12:20:10 -0400

    Corrected port usage

--- SSH_tunneling_for_internalPen.sh
+++ SSH_tunneling_for_internalPen.sh
@@ -4,14 +4,15 @@
 
 # Usage function
 usage() {
-    echo "Usage: $0 hostname.domain.com [-D <local_socks_port>] [-R <remote_port>] [-N <nessus_port>]"
+    echo "Usage: $0 hostname.domain.com [-O <remote_outbound_via_local_socks_port>] [-I <local_into_remote_network_via_local_socks_port>] [-N <nessus_port>] [-R <remote_into_local_port>]"
     exit 1
 }
 
 # Default ports
-D_PORT=9090
-R_PORT=9051
+O_PORT=9050
+I_PORT=9999
 N_PORT=8834
+R_PORT=7171
 
 # Check if the hostname argument is provided
 if [ -z "$1" ]; then
@@ -24,9 +25,10 @@ shift
 # Parse optional arguments
 while getopts "D:R:N:" opt; do
     case ${opt} in
-        D) D_PORT=$OPTARG;;
-        R) R_PORT=$OPTARG;;
+        O) O_PORT=$OPTARG;;
+        I) I_PORT=$OPTARG;;
         N) N_PORT=$OPTARG;;
+        R) R_PORT=$OPTARG;;
         *) usage;;
     esac
 done
@@ -43,9 +45,15 @@ run_ssh_command() {
 # Run the SSH commands with error checking
 echo -e "\n\n***Prepare to paste the SSH account and/or key password multiple times***"
 # Allow the remote host to proxychains through localhost
-echo -e "\nCreating SSH tunnel to allow proxychains through localhost:"
-run_ssh_command ssh -fND $D_PORT root@127.0.0.1
-run_ssh_command ssh -fNR $D_PORT:localhost:$D_PORT root@$RHOST
+echo -e "\nCreating SSH tunnel to allow proxychains out of the client network through localhost:"
+run_ssh_command ssh -fND $O_PORT root@127.0.0.1
+run_ssh_command ssh -fNR $O_PORT:localhost:$O_PORT root@$RHOST
+
+
+# Allow proxychains to access the remote host
+echo -e "\nCreating SSH tunnel to allow proxychains to access the remote host:"
+run_ssh_command ssh -fND $I_PORT root@$RHOST 
+
 
 # Map the remote nessus 8834 port to the local 8834 port
 echo -e "\nCreating SSH tunnel to map remote nessus port to local port:"
@@ -53,6 +61,4 @@ run_ssh_command ssh -fNL $N_PORT:127.0.0.1:$N_PORT root@$RHOST
 
 # This can be used if you need to let the remote host access a local service
 # As this is not a common use case, it is commented out
-#run_ssh_command ssh -fNR 127.0.0.1:$R_PORT root@$RHOST
-
-#echo "SSH commands executed successfully on $RHOST with -D $D_PORT, -N $N_PORT, and -R $R_PORT"
+run_ssh_command ssh -fNR 127.0.0.1:$R_PORT root@$RHOST
